6ebd02d94808b6257241431c8860db6ede1c3d22
unknown <lucia.gangi@mi.unc.edu.ar>
2022-06-29 17:41:55 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import logoFreeCodeCamp from "./imagenes/freecodecamp-logo.png";
+import ListaDeTareas from "./componentes/ListaDeTareas";
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+      <div className="tareas-lista-principal">
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas/>  
+      </div>
+    </div>
+  );
+}
+
+export default App;
